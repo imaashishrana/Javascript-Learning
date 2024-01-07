30f25044d80a20c359b6b164976be1ccdb00c001
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-01-07 09:14:00 +0000

    type conversion opertions

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -16,7 +16,7 @@
 //true=>boolean(1)
 //false=>0
 
-// let isLoggedIn = "ashish"//true
+let isLoggedIn = "ashish"//true
 // let isLoggedIn = ""//false
 
 // let booleanIsLoggedIn=Boolean(isLoggedIn)
@@ -27,7 +27,34 @@
 //"ashish"=>true
 //""=>false
 
-let someNumber = 33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//----Operations---------------
+
+// let x=34
+// let negx= -x
+// console.log(negx);
+
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//means 2 power 3=2*2*2==8
+// console.log(2/2);//1
+// console.log(2%3);//2
+
+
+//prefix and postfix
+
+let a=9
+++a;
+console.log(a);
+a++
+console.log(a);
+a--;
+console.log(a);
+--a;
+console.log(a);

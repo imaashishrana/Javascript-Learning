f698c40a187cec438fca4c7c20d5026fdbfbd6e1
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-01-07 07:52:28 +0000

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,12 +1,20 @@
 // console.log("hitesh")
 
-let name="ashish"
-let age=24
-let isLoggedIn = false
+let name="ashish"//string
+let age=24//number
+let isLoggedIn = false//boolean
+let state;//undefined
 
 //number=>2 to power 53
 //bigint
 //string=>""
 //bolean=>true/false
-//null => standalone value
+//null => standalone value //object
 //undefined => 
+//symbol=>unique
+
+//object=>null
+
+console.log(typeof "ashish");
+console.log(typeof (false));
+console.log(typeof undefined);

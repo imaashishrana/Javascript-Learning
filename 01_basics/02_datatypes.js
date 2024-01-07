a13a78d8495f9500706cd59c93452b0d9650a551
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-01-07 08:06:56 +0000

    datatypes object configure

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,9 +1,10 @@
-// console.log("hitesh")
-
+//datatypes =>8 
+//string number bigint boolean undefined null symbol object//
 let name="ashish"//string
 let age=24//number
 let isLoggedIn = false//boolean
 let state;//undefined
+let x = BigInt("123456789012345678901234567890");
 
 //number=>2 to power 53
 //bigint
@@ -15,6 +16,22 @@ let state;//undefined
 
 //object=>null
 
-console.log(typeof "ashish");
-console.log(typeof (false));
-console.log(typeof undefined);
+// console.log(typeof "ashish");
+// console.log(typeof (false));
+// console.log(typeof undefined);
+
+//object datatypes =>object ,array,date
+
+//object
+const person={FirstName:"Ashish",lastName:"Kumar"};
+
+//array object
+const car=["vitara","brezza","maruti"];
+
+//dateobject
+
+const date=new Date("2024-01-07");
+
+// console.log(date)
+// console.log(car[0],car[1],car[2]);
+console.log(person.FirstName,person.lastName);

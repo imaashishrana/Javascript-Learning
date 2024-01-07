19f9bde01d94f0006b3dc5d27dc8e1feb238b627
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-01-07 08:40:17 +0000

    typeconversion in js

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,33 @@
+//type conversion in js
+
+// let score=33;//number
+// let score = "33"//string
+// let score="33abc"//NaN
+// let score ="ashish";//STRING
+
+// console.log(typeof (score));//2nd way
+
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>boolean(1)
+//false=>0
+
+// let isLoggedIn = "ashish"//true
+// let isLoggedIn = ""//false
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true
+//0=>false
+//"ashish"=>true
+//""=>false
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

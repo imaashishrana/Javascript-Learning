21d812520b976c908b4d130a08aa0aa1e5f91321
Ashish Kumar <154793666+imaashishrana@users.noreply.github.com>
2024-01-07 07:23:44 +0000

    basics configure in file

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,10 @@
+const accountId=2563
+let accountEmail="aas@gmail.com"
+var accountPassword="1234"
+accountCity="Haryana"
+
+//accountId=6376564//not allowed you cannot initialise again const value
+accountEmail="hello@cu.com"
+accountPassword="6735"
+accountCity="jaipur"
+console.table([accountEmail,accountId,accountPassword,accountCity])
